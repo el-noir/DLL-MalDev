89039467e9cbbbb0b0f3e7795e812c4a4dc38e93
mudasirshah434 <mudasirshah9777@gmail.com>
2025-02-16 11:20:29 +0500

    fixed error with loading dll file

--- dll-loader.cpp
+++ dll-loader.cpp
@@ -1,24 +1,31 @@
 #include <Windows.h>
 #include <stdio.h>
-#include <string>
-using namespace std;
 
- int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     // Check if arguments are provided
     if (argc < 2) {
-       // printf("Usage: %s <file_path>\n", argv[0]);
-        printf("Run : dll-loader.exe <file.dll>\n");
+        printf("Usage: dll-loader.exe <file.dll>\n");
         return EXIT_FAILURE;
     }
 
-   // get parsed file
-    LPCTSTR dll_file = argv[1];
-    std:: string file_name = argv[1];
-    printf("Loading %s...\n", file_name);
+    // Get DLL file path
+    const char* dll_file = argv[1];
+    printf("Loading %s...\n", dll_file);
 
-    //load dll
-    HINSTANCE hDLL = LoadLibrary(TEXT(dll_file));
+    // Load DLL
+    HINSTANCE hDLL = LoadLibraryA(dll_file);  // Use LoadLibraryA for ANSI
+
+    // Error handling
+    if (!hDLL) {
+        printf("Failed to load DLL. Error code: %lu\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    printf("DLL successfully loaded!\n");
+
+    // Free the DLL after use
+    FreeLibrary(hDLL);
     
     return EXIT_SUCCESS;
+}
 
- }
